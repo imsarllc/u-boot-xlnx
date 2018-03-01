0df6a593028d3e286424902eaed05fe1f30491ee
Derrick Gibelyou <derrick.gibelyou@imsar.com>
2018-03-01 12:02:50 -0700

    Add missing null character.

--- include/configs/zynq_grizzly.h
+++ include/configs/zynq_grizzly.h
@@ -29,7 +29,7 @@
 	"qspiboot=run read_fpga && run load_bit || " \
 		"echo FPGA load failed && setenv devicetree_image fallback.dtb\0" \
 	"read_fpga=echo Loading FPGA from QSPI && sf probe 0 0 0 && sf read ${fpga_buff} ${fpga_offset} ${bitsz}\0" \
-	"reset_qspi_addr=sf probe 0 0 0 && sf read 0x0100000 0x100000 1" \
+	"reset_qspi_addr=sf probe 0 0 0 && sf read 0x0100000 0x100000 1\0" \
 	"sdboot=echo Copying Linux from SD to RAM... && " \
 		"mmcinfo && " \
 		"fatload mmc 0 0x3000000 ${kernel_image} && " \
